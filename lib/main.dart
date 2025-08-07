aae3e320d838344d7156bb44abda0c6295df16cf
Supannika Thawong <Supannikat66@nu.ac.th>
2025-08-07 16:23:11 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<String> items = List<String>.generate(100, (i) => 'Item ${i + 1}');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chapter6'),
+        backgroundColor: Colors.pinkAccent,
+      ),
+      body: GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          childAspectRatio: 3 / 2,
+        ),
+        itemCount: items.length,
+        itemBuilder: (BuildContext context, int idx) {
+          return Container(
+            color: Colors.blue[100 + (idx % 6) * 100],
+            child: Text(items[idx]),
+          );
+        },
+      ),
+    );
+  }
+}
